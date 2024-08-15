a6b878809b249324ecd666350e247efab45f8cca
ahokinson <1762048+ahokinson@users.noreply.github.com>
2024-08-15 13:22:08 -0400

    Clearer connection flow

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -3,7 +3,6 @@ package database
 import (
 	"context"
 	"errors"
-	"sync"
 	"time"
 
 	"github.com/jackc/pgx/v5"
@@ -14,10 +13,7 @@ import (
 	"directory/pkg/logger"
 )
 
-var instance Pool
-var mu sync.Mutex
-
-type db struct {
+type connection struct {
 	DB Pool
 
 	readTimeout  time.Duration
@@ -31,83 +27,65 @@ type Pool interface {
 	QueryRow(ctx context.Context, sql string, arguments ...any) pgx.Row
 }
 
-func (p *db) Querier() Pool {
-	return p.DB
+func (c *connection) Querier() Pool {
+	return c.DB
 }
 
-func (p *db) Ping(ctx context.Context) (err error) {
+func (c *connection) Ping(ctx context.Context) (err error) {
 	pingCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
-	_, err = p.DB.Exec(pingCtx, ";")
+	_, err = c.DB.Exec(pingCtx, ";")
 	if err != nil {
 		logger.Warn(ctx, "database ping failed", err)
 	}
 	return
 }
 
-func (p *db) Exec(ctx context.Context, sql string, arguments ...any) (commandTag pgconn.CommandTag, err error) {
-	return p.DB.Exec(ctx, sql, arguments...)
+func (c *connection) Exec(ctx context.Context, sql string, arguments ...any) (commandTag pgconn.CommandTag, err error) {
+	return c.DB.Exec(ctx, sql, arguments...)
 }
 
-func (p *db) Query(ctx context.Context, sql string, arguments ...any) (rows pgx.Rows, err error) {
-	return p.DB.Query(ctx, sql, arguments...)
+func (c *connection) Query(ctx context.Context, sql string, arguments ...any) (rows pgx.Rows, err error) {
+	return c.DB.Query(ctx, sql, arguments...)
 }
 
-func (p *db) QueryRow(ctx context.Context, sql string, arguments ...any) pgx.Row {
-	return p.DB.QueryRow(ctx, sql, arguments...)
+func (c *connection) QueryRow(ctx context.Context, sql string, arguments ...any) pgx.Row {
+	return c.DB.QueryRow(ctx, sql, arguments...)
 }
 
 func Connect(cfg *config.Config) (pool Pool, err error) {
 	ctx := context.Background()
 
-	mu.Lock()
-	defer mu.Unlock()
-
-	if instance != nil {
-		pool = instance
-		return
-	}
-
-	s := &db{
-		readTimeout:  cfg.Database.ReadTimeout,
-		writeTimeout: cfg.Database.WriteTimeout,
-	}
-	pool = s
-
 	pgxConfig, err := pgxpool.ParseConfig(cfg.Database.Source)
 	if err != nil {
 		return
 	}
-
 	pgxConfig.MaxConnLifetime = cfg.Database.MaxConnLifetime
 	pgxConfig.MaxConns = int32(cfg.Database.MaxConnections)
 	pgxConfig.ConnConfig.ConnectTimeout = cfg.Database.ConnectTimeout
 
-	s.DB, err = pgxpool.NewWithConfig(ctx, pgxConfig)
+	connect := &connection{
+		readTimeout:  cfg.Database.ReadTimeout,
+		writeTimeout: cfg.Database.WriteTimeout,
+	}
+	connect.DB, err = pgxpool.NewWithConfig(ctx, pgxConfig)
 	if err != nil {
 		return
 	}
 
-	err = ping(s)
-	if err != nil {
-		return
-	}
-	instance = pool
-
-	logger.Info(ctx, "database connected")
-	return
-}
-
-func ping(s Pool) (err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
-
 	for {
-		err = s.Ping(ctx)
+		err = connect.Ping(ctx)
 		if err == nil || errors.Is(ctx.Err(), context.DeadlineExceeded) {
-			return
+			break
 		}
 		time.Sleep(time.Second)
 	}
+
+	pool = connect
+	logger.Info(ctx, "database connected")
+
+	return
 }
