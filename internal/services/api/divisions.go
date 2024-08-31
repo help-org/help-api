951d2678c1c9bde6d72f87f690254c6520333014
Paul Thompson <paul@findingfocus.dev>
2024-08-31 10:29:39 -0600

    add Update method for division

--- internal/services/api/divisions.go
+++ internal/services/api/divisions.go
@@ -26,6 +26,7 @@ func (s *DivisionService) RegisterRoutes(mux *chi.Mux) {
 	mux.Get("/divisions/{id}", s.FindByID)
 	mux.Post("/divisions", s.Create)
 	mux.Delete("/divisions/{id}", s.Delete)
+	mux.Put("/divisions/{id}", s.Update)
 }
 
 func (s *DivisionService) Create(w http.ResponseWriter, r *http.Request) {
@@ -101,27 +102,80 @@ func (s *DivisionService) FindByID(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (s *DivisionService) Update(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, "division id is invalid", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to update request body", http.StatusInternalServerError)
+		return
+	}
+
+	var data map[string]interface{}
+
+	if err := json.Unmarshal(body, &data); err != nil {
+		http.Error(w, "Failed to unmarshal body as JSON", http.StatusBadRequest)
+		return
+	}
+
+	division := types.Division{Id: id}
+	if data["name"] != nil {
+		division.Name = data["name"].(string)
+	}
+	if data["type"] != nil {
+		division.Type = data["type"].(string)
+	}
+	if data["parent_id"] != nil {
+		parentId := int(data["parent_id"].(float64))
+		division.ParentId = &parentId
+	}
+
+	_, err = s.divisionStore.Update(ctx, division)
+	if err != nil {
+		http.Error(w, "Failed to update division", http.StatusInternalServerError)
+		return
+	}
+
+	response, err := json.Marshal(division)
+	if err != nil {
+		http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write(response)
+
+	if err != nil {
+		http.Error(w, "Error updating division", http.StatusInternalServerError)
+		return
+	}
+}
+
 func (s *DivisionService) Delete(w http.ResponseWriter, r *http.Request) {
-    ctx := r.Context()
+	ctx := r.Context()
 
-    id, err := strconv.Atoi(chi.URLParam(r, "id"))
-    if err != nil {
-        http.Error(w, "Invalid Division ID", http.StatusBadRequest)
-        return
-    }
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, "Invalid Division ID", http.StatusBadRequest)
+		return
+	}
 
-    err = s.divisionStore.Delete(ctx, id)
-    if err != nil {
-        if err.Error() == "no division found with id " + strconv.Itoa(id) {
-            http.Error(w, "Division not found", http.StatusNotFound)
-        } else {
-            http.Error(w, "Error deleting division", http.StatusInternalServerError)
-        }
-        return
-    }
+	err = s.divisionStore.Delete(ctx, id)
+	if err != nil {
+		if err.Error() == "no division found with id "+strconv.Itoa(id) {
+			http.Error(w, "Division not found", http.StatusNotFound)
+		} else {
+			http.Error(w, "Error deleting division", http.StatusInternalServerError)
+		}
+		return
+	}
 
-//     w.WriteHeader(http.StatusNoContent)
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    _, _ = w.Write([]byte(`{"message": "Division deleted successfully"}`))
+	//     w.WriteHeader(http.StatusNoContent)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(`{"message": "Division deleted successfully"}`))
 }
--- internal/store/database/divisions.go
+++ internal/store/database/divisions.go
@@ -27,23 +27,27 @@ func (s *DivisionStore) Create(ctx context.Context, division types.Division) (id
 	return
 }
 
-// Delete removes a division from the database by its ID.
 func (s *DivisionStore) Delete(ctx context.Context, id int) (err error) {
-    execResult, err := s.store.Exec(ctx, deleteQuery, id)
-    if err != nil {
-        return err
-    }
+	execResult, err := s.store.Exec(ctx, deleteQuery, id)
+	if err != nil {
+		return err
+	}
 
-    if execResult.RowsAffected() == 0 {
-        return fmt.Errorf("no division found with id %d", id)
-    }
-
-    return
+	if execResult.RowsAffected() == 0 {
+		return fmt.Errorf("no division found with id %d", id)
+	}
+	return
 }
 
+func (s *DivisionStore) Update(ctx context.Context, division types.Division) (id int, err error) {
+	err = s.store.QueryRow(ctx, updateQuery, division.Name, division.Type, division.ParentId).Scan(&id)
+	return
+}
 
 const createQuery = "INSERT INTO directory.divisions (name, type, parent_id) VALUES ($1, $2, $3) RETURNING id"
 
 const findByIDQuery = "SELECT d.id, d.name, d.type, d.parent_id FROM directory.divisions d WHERE id = $1"
 
 const deleteQuery = "DELETE FROM divisions WHERE id = $1"
+
+const updateQuery = "UPDATE directory.divisions SET name = $1, type = $2, parent_id = $3 WHERE id = $4 RETURNING id"
